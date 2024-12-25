4ad77708fa926b83760a7a887f61b6080479701a
Hasan Rahman <hasanrahman33605@gmail.com>
2024-12-25 18:53:31 +0600

    init

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Booking Bhai | A booking app for your spaces",
+  description: "Booking Bhai | a booking app for your spaces",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>Hi ,hello this is hasan bhai you host</div>;
+}
