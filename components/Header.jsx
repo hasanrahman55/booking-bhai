482dd3a005fab3fed6df8accf450ff0b327febe0
Hasan Rahman <hasanrahman33605@gmail.com>
2025-01-05 20:43:59 +0600

    add room

--- components/Header.jsx
+++ components/Header.jsx
@@ -56,7 +56,7 @@ const Header = () => {
                       </Link>
 
                       <Link
-                        href="/room/add"
+                        href="/rooms/add"
                         className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
                       >
                         Add Room
