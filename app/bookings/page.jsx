98aaf791d119cf3b7f688d90cd4bff005c507995
Hasan Rahman <hasanrahman33605@gmail.com>
2025-01-07 22:26:12 +0600

    booked room

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import Heading from "@/components/Heading";
+import BookedRoomCard from "@/components/BookedRoomCard";
+import getMyBookings from "../actions/getMyBooking";
 
-function Bookings() {
-  return <div>Bookings</div>;
-}
+const BookingsPage = async () => {
+  const bookings = await getMyBookings();
 
-export default Bookings;
+  return (
+    <>
+      <Heading title="My Bookings" />
+      {bookings.length === 0 ? (
+        <p className="text-gray-600 mt-4">You have no bookings</p>
+      ) : (
+        bookings.map((booking) => (
+          <BookedRoomCard key={booking.$id} booking={booking} />
+        ))
+      )}
+    </>
+  );
+};
+
+export default BookingsPage;
